4d99a51a47577ffed8a08ef915bbfdd5dd7bcbe1
KOITT-02-A <KOITT-02-A@KOITT-02-A-PC>
2018-09-29 14:32:02 +0900

    add

--- mutube/src/Post/Model/PostContent.java
+++ mutube/src/Post/Model/PostContent.java
@@ -3,4 +3,20 @@ package Post.Model;
 public class PostContent {
 	private int postId;
 	private String content;
+
+	public int getPostId() {
+		return postId;
+	}
+
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
