85d9ed5ce9ac4ceff01cebf28c3aabd66db8bb40
KOITT-02-A <KOITT-02-A@KOITT-02-A-PC>
2018-09-29 15:30:08 +0900

    add UserDAO

---
+++ mutube/src/User/DAO/UserDAO.java
@@ -0,0 +1,13 @@
+package User.DAO;
+
+public class UserDAO {
+	private static UserDAO instance = new UserDAO();
+	
+	private UserDAO() {}
+	
+	public static UserDAO getInstance() {
+		return instance;
+	}
+	
+	
+}
