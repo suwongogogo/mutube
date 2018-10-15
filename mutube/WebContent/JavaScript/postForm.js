483ad7d4520e01c9239b5f3495fa620c0b2a883a
이수원 <sobok1414@hotmail.com>
2018-10-15 19:11:51 +0900

    환경이 같냐고~~~

--- mutube/WebContent/JavaScript/postForm.js
+++ mutube/WebContent/JavaScript/postForm.js
@@ -2,6 +2,7 @@ function move() {
 	var form = document.getElementById("form");
 	form.submit();
 }
+
 function cancle() {
     var check = confirm("현재 페이지에서 이동하면 작성 중인 사항은 모두 사라집니다.");
     if(check) {
